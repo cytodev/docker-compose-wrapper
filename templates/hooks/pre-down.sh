c0b9711ca442d7e8e36262a810814b7100c47d5d
Roel Walraven <mail@cytodev.io>
2021-03-09 15:18:13 +0100

    Add description of exposed environment variables

--- templates/hooks/pre-down.sh
+++ templates/hooks/pre-down.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+#══════════════════════════════════════════════════════════════════════════════#
+# exposed environment variables:                                               #
+#───────────────────┬──────────────────────────────────────────────────────────#
+# CONTAINER_NAME    │ Name of the container used in the containers/ directory  #
+# CONTAINER_ENV     │ Environment file currently in use by the container       #
+# CONTAINER_VERSION │ The version as specified by the environment file         #
+#───────────────────┴──────────────────────────────────────────────────────────#
+
+
